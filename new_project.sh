49737d1c69f536a36e7f4d682644420e3d40ff9d
dante7qx <sunchao.0129@163.comm>
2022-07-30 15:32:48 +0800

    添加新项目创建脚本

--- new_project.sh
+++ new_project.sh
@@ -2,45 +2,55 @@
 
 curProject="project"
 curProjectCN="睿阳Java开发框架"
-projectName=$1
+projectName="xx-sys"
 projectCN="XX管理系统"
 modifys=()
 
 createNewProject() {
-    if [[ -z $projectName ]]; then
-        echo "Usage: bash new_project.sh <你的项目名（英文）>"
-        exit 1
-    else 
-        ## 修改pom.xml
-        echo "1. 修改pom.xml"
-        poms=`find ./ -type f -name "pom.xml"`
-        for pom in $poms; do 
-        	echo $pom
-            sed -i "s/<artifactId>${curProject}<\/artifactId>/<artifactId>${projectName}<\/artifactId>/g" $pom
-            sed -i "s/${curProject}-/${projectName}-/g" $pom
-        done
-        
-        ## 修改睿阳Java开发框架
-        echo "2. 修改${curProjectCN}"
-        cnfiles=`grep ${curProjectCN} -rl ./ | grep -v new_project.sh | grep -v new_project_mac.sh | grep -v git`
-        for cnfile in $cnfiles; do
-            echo $cnfile
-            sed -i "s/${curProjectCN}/${projectCN}/g" $cnfile
-        done
+    ## 修改pom.xml
+    echo "1. 修改pom.xml"
+    poms=`find ./ -type f -name "pom.xml"`
+    for pom in $poms; do 
+    	echo $pom
+        sed -i "s/<artifactId>${curProject}<\/artifactId>/<artifactId>${projectName}<\/artifactId>/g" $pom
+        sed -i "s/${curProject}-/${projectName}-/g" $pom
+    done
+    
+    ## 修改睿阳Java开发框架
+    echo "2. 修改${curProjectCN}"
+    cnfiles=`grep ${curProjectCN} -rl ./ | grep -v new_project.sh | grep -v new_project_mac.sh | grep -v git`
+    for cnfile in $cnfiles; do
+        echo $cnfile
+        sed -i "s/${curProjectCN}/${projectCN}/g" $cnfile
+    done
 
-        ## 重命名目录
-        echo "3. 重命名目录"
-        projectDirs=`find ./ -type d -name "${curProject}-*"`
-        for pdir in $projectDirs; do
-            echo "mv ${pdir} ${projectName}${pdir#*${curProject}}"
-            mv ${pdir} ${projectName}${pdir#*${curProject}}
-        done
-        
-        ## 删除git相关
-        rm -rf .git/
-        rm -rf .gitignore
-
-    fi
+    ## 重命名目录
+    echo "3. 重命名目录"
+    projectDirs=`find ./ -type d -name "${curProject}-*"`
+    for pdir in $projectDirs; do
+        echo "mv ${pdir} ${projectName}${pdir#*${curProject}}"
+        mv ${pdir} ${projectName}${pdir#*${curProject}}
+    done
+    
+    ## 删除git相关
+    rm -rf .git/
+    rm -rf .gitignore
 }
 
-createNewProject
+read -r -p "您确定要执行此操作吗? [Y/n] " input
+
+case $input in
+    [yY][eE][sS]|[yY])
+		createNewProject
+		;;
+
+    [nN][oO]|[nN])
+		echo "不执行"
+		exit 1
+       	;;
+
+    *)
+		echo "Invalid input..."
+		exit 1
+		;;
+esac
