7a08f745a4d69a0c76b85ac2271b3b585d7bca6f
dante7qx <sunchao.0129@163.comm>
2022-07-30 15:21:18 +0800

    添加新项目创建脚本

--- new_project.sh
+++ new_project.sh
@@ -12,19 +12,24 @@ createNewProject() {
         exit 1
     else 
         ## 修改pom.xml
+        echo "1. 修改pom.xml"
         poms=`find ./ -type f -name "pom.xml"`
         for pom in $poms; do 
+        	echo $pom
             sed -i "s/<artifactId>${curProject}<\/artifactId>/<artifactId>${projectName}<\/artifactId>/g" $pom
             sed -i "s/${curProject}-/${projectName}-/g" $pom
         done
         
-        ## 修改XX管理系统
+        ## 修改睿阳Java开发框架
+        echo "2. 修改${curProjectCN}"
         cnfiles=`grep ${curProjectCN} -rl ./ | grep -v new_project.sh | grep -v new_project_mac.sh | grep -v git`
         for cnfile in $cnfiles; do
+            echo $cnfile
             sed -i "s/${curProjectCN}/${projectCN}/g" $cnfile
         done
 
         ## 重命名目录
+        echo "3. 重命名目录"
         projectDirs=`find ./ -type d -name "${curProject}-*"`
         for pdir in $projectDirs; do
             echo "mv ${pdir} ${projectName}${pdir#*${curProject}}"
@@ -34,13 +39,6 @@ createNewProject() {
         ## 删除git相关
         rm -rf .git/
         rm -rf .gitignore
-        
-        echo "1. 修改pom.xml"
-        echo $poms
-        echo "2. 修改${curProjectCN}"
-        echo $cnfiles
-        echo "3. 重命名目录"
-        echo $projectDirs
 
     fi
 }
