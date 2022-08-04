17dee8381c4f217331c23afbc038d4a585d727c9
dante7qx <sunchao.0129@163.comm>
2022-08-04 16:12:02 +0800

    修改el-input、textarea 禁用状态样式

--- project-ui/src/components/FileUpload/index.vue
+++ project-ui/src/components/FileUpload/index.vue
@@ -13,11 +13,12 @@
       :headers="headers"
       class="upload-file-uploader"
       ref="upload"
+      v-if="!disabled"
     >
       <!-- 上传按钮 -->
-      <el-button size="mini" type="primary" :disabled="disabled">选取文件</el-button>
+      <el-button size="mini" type="primary" v-if="!disabled">选取文件</el-button>
       <!-- 上传提示 -->
-      <div class="el-upload__tip" slot="tip" v-if="showTip">
+      <div class="el-upload__tip" slot="tip" v-if="showTip && !disabled">
         请上传
         <template v-if="fileSize"> 大小不超过 <b style="color: #f56c6c">{{ fileSize }}MB</b> </template>
         <template v-if="fileType"> 格式为 <b style="color: #f56c6c">{{ fileType.join("/") }}</b> </template>
