05396752375f22e3fc2d01b493be0751b8aea56c
dante7qx <sunchao.0129@163.comm>
2022-08-05 08:16:57 +0800

    修改健康检查

--- project-admin/src/main/java/com/risun/web/controller/common/HealthCheckController.java
+++ project-admin/src/main/java/com/risun/web/controller/common/HealthCheckController.java
@@ -3,6 +3,8 @@ package com.risun.web.controller.common;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.risun.common.annotation.Anonymous;
+
 /**
  * 健康检查
  * 
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheckController {
 	
+	@Anonymous
 	@GetMapping("/health_check")
 	public String healthCheck() {
 		return "UP";
