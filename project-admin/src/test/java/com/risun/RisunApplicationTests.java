84939c7d4fb7d1f0f62f034c890d078da8d67744
dante7qx <sunchao.0129@163.comm>
2022-08-13 12:54:02 +0800

    添加测试用例

---
+++ project-admin/src/test/java/com/risun/RisunApplicationTests.java
@@ -0,0 +1,8 @@
+package com.risun;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RisunApplicationTests {
+	
+}
---
+++ project-admin/src/test/java/com/risun/biz/demo/mapper/DemoMapperTests.java
@@ -0,0 +1,20 @@
+package com.risun.biz.demo.mapper;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.risun.RisunApplicationTests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class DemoMapperTests extends RisunApplicationTests {
+	
+	@Autowired
+	private DemoMapper demoMapper;
+	
+	@Test
+	public void selectDemoCount() {
+		int count = demoMapper.selectDemoCount();
+		assertTrue(count >= 0);
+	}
+}
