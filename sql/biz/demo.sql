7481021d503b5566b7f2797c42d31dfb230ad6e4
dante7qx <sunchao.0129@163.comm>
2022-08-18 08:03:08 +0800

    修改demo示例

--- sql/biz/demo.sql
+++ sql/biz/demo.sql
@@ -1,4 +1,3 @@
-
 drop table if exists t_demo;
 create table t_demo (
   demo_id bigint(20) not null auto_increment comment '业务主键ID',
@@ -6,10 +5,10 @@ create table t_demo (
   demo_time datetime comment '业务时间',
   demo_image varchar(2048) comment '业务图片',
   attachment varchar(2048) comment '业务附件',
-  demo_content text default '' comment '业务名称',
+  demo_content text comment '业务名称',
   create_by varchar(64) default '' comment '创建者',
   create_time datetime comment '创建时间',
   update_by varchar(64) default '' comment '更新者',
   update_time datetime comment '更新时间',
-  primary key (biz_id)
+  primary key (demo_id)
 ) engine=innodb auto_increment=200 comment = '业务表';
