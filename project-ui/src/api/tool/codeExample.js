d931d5005e7426d41ae3d86654ab986152e94400
dante7qx <sunchao.0129@163.comm>
2022-08-19 15:08:39 +0800

    完善地区下拉框示例

--- project-ui/src/api/tool/codeExample.js
+++ project-ui/src/api/tool/codeExample.js
@@ -2,17 +2,17 @@ import request from '@/utils/request'
 
 // 查询详情
 export function getDataDetail(key) {
-    return request({
-      url: '/tool/codeexample/key/' + key,
-      method: 'get'
-    })
-  }
+  return request({
+    url: '/tool/codeexample/key/' + key,
+    method: 'get'
+  })
+}
   
-  // 提交表单
-  export function submitData(data) {
-    return request({
-      url: '/tool/codeexample/submit',
-      method: 'post',
-      data: data
-    })
-  }
+// 提交表单
+export function submitData(data) {
+  return request({
+    url: '/tool/codeexample/submit',
+    method: 'post',
+    data: data
+  })
+}
