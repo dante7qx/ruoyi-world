f1e792895ddfd86a7b624d7d3c0eb803336ec2d0
dante7qx <sunchao.0129@163.comm>
2022-11-22 21:38:39 +0800

    修改健康检查跨域设置

--- project-admin/src/main/java/com/risun/web/controller/common/HealthCheckController.java
+++ project-admin/src/main/java/com/risun/web/controller/common/HealthCheckController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  */
 @RestController
-@CrossOrigin(origins = "*",maxAge = 3600)
+@CrossOrigin(originPatterns = "*",maxAge = 3600)
 public class HealthCheckController {
 	
 	@Anonymous
