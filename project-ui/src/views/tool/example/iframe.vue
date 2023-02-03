e5befae35d2920e1c7df1983996be89cff9e0bb9
dante7qx <sunchao.0129@163.comm>
2023-02-03 11:25:32 +0800

    添加iframe标签示例

---
+++ project-ui/src/views/tool/example/iframe.vue
@@ -0,0 +1,16 @@
+<template>
+  <iframe class="iframe" src="https://www.cncf.io/" frameborder="0" scrolling="auto"></iframe>
+</template>
+
+<script>
+export default {
+  name: "iFrameExample"
+}
+</script>
+
+<style scoped>
+.iframe{
+  height: calc(100vh - 80px);
+  width: 100%;
+}
+</style>
