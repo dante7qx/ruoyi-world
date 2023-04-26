a33d91ea23601b4b9966d804244f8b0235e8c66d
dante7qx <sunchao.0129@163.comm>
2023-04-26 15:56:17 +0800

    优化打印语句

--- project-quartz/src/main/java/com/risun/quartz/task/RyTask.java
+++ project-quartz/src/main/java/com/risun/quartz/task/RyTask.java
@@ -2,6 +2,7 @@ package com.risun.quartz.task;
 
 import org.springframework.stereotype.Component;
 
+import cn.hutool.core.lang.Console;
 import cn.hutool.core.util.StrUtil;
 
 /**
@@ -14,16 +15,16 @@ public class RyTask
 {
     public void ryMultipleParams(String s, Boolean b, Long l, Double d, Integer i)
     {
-        System.out.println(StrUtil.format("执行多参方法： 字符串类型{}，布尔类型{}，长整型{}，浮点型{}，整形{}", s, b, l, d, i));
+    	Console.log(StrUtil.format("执行多参方法： 字符串类型{}，布尔类型{}，长整型{}，浮点型{}，整形{}", s, b, l, d, i));
     }
 
     public void ryParams(String params)
     {
-        System.out.println("执行有参方法：" + params);
+    	Console.log("执行有参方法：" + params);
     }
 
     public void ryNoParams()
     {
-        System.out.println("执行无参方法");
+    	Console.log("执行无参方法");
     }
 }
