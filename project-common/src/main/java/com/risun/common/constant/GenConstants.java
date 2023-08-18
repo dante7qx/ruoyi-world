bcca07c8166912bd0ce32496d4358e9874ce4c89
dante7qx <sunchao.0129@163.comm>
2023-08-18 11:52:06 +0800

    代码生成 - 左侧部门树单表

--- project-common/src/main/java/com/risun/common/constant/GenConstants.java
+++ project-common/src/main/java/com/risun/common/constant/GenConstants.java
@@ -9,6 +9,9 @@ public class GenConstants
 {
     /** 单表（增删改查） */
     public static final String TPL_CRUD = "crud";
+    
+    /** 左部门树单表（增删改查） */
+    public static final String TPL_LEFT_DEPT_TREE_CRUD = "depttreecrud";
 
     /** 树表（增删改查） */
     public static final String TPL_TREE = "tree";
--- project-ui/src/views/tool/gen/genInfoForm.vue
+++ project-ui/src/views/tool/gen/genInfoForm.vue
@@ -4,8 +4,9 @@
       <el-col :span="12">
         <el-form-item prop="tplCategory">
           <span slot="label">生成模板</span>
-          <el-select v-model="info.tplCategory" @change="tplSelectChange">
+          <el-select v-model="info.tplCategory" @change="tplSelectChange" style="width: 100%;">
             <el-option label="单表（增删改查）" value="crud" />
+            <el-option label="左部门树单表（增删改查）" value="depttreecrud" />
             <el-option label="树表（增删改查）" value="tree" />
             <el-option label="主子表（增删改查）" value="sub" />
           </el-select>
