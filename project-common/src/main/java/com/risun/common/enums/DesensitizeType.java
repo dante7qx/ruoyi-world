36a0e568b34a6ee4cbb33b2a90af857c587ffe0a
dante7qx <sunchao.0129@163.comm>
2023-09-06 18:10:12 +0800

    数据脱敏优化

--- project-common/src/main/java/com/risun/common/enums/DesensitizeType.java
+++ project-common/src/main/java/com/risun/common/enums/DesensitizeType.java
@@ -1,5 +1,10 @@
 package com.risun.common.enums;
 
+import com.risun.common.annotation.DesensitizeField;
+
+import cn.hutool.core.text.CharSequenceUtil;
+import cn.hutool.core.util.DesensitizedUtil;
+import cn.hutool.core.util.StrUtil;
 import lombok.Getter;
 
 /**
@@ -7,37 +12,103 @@ import lombok.Getter;
  */
 @Getter
 public enum DesensitizeType {
-
-	/** 加密入库 */
-	DB,
 	
+	
+	/** 加密入库 */
+	DB {
+		/**
+		 * 单独处理
+		 */
+		@Override
+		public String desensitive(String source, DesensitizeField annotation) {
+			return "";
+		}
+	},
+
 	/** 姓名 */
-    NAME,
+    NAME {
+		@Override
+		public String desensitive(String source, DesensitizeField annotation) {
+			return DesensitizedUtil.chineseName(source);
+		}
+	},
     
     /** 用户Id */
-    USER_ID,
+    USER_ID {
+		@Override
+		public Long desensitive(String source, DesensitizeField annotation) {
+			return DesensitizedUtil.userId();
+		}
+	},
 
     /** 身份证号 */
-    ID_CARD,
+    ID_CARD {
+		@Override
+		public String desensitive(String source, DesensitizeField annotation) {
+			return DesensitizedUtil.idCardNum(source, 3, 3);
+		}
+	},
 
     /** 手机号 */
-    PHONE,
+    PHONE {
+		@Override
+		public String desensitive(String source, DesensitizeField annotation) {
+			return DesensitizedUtil.mobilePhone(source);
+		}
+	},
     
     /** 固定电话 */
-    FIX_PHONE,
+    FIX_PHONE {
+		@Override
+		public String desensitive(String source, DesensitizeField annotation) {
+			return DesensitizedUtil.fixedPhone(source);
+		}
+	},
 
     /** 邮箱 */
-    EMAIL,
+    EMAIL {
+		@Override
+		public String desensitive(String source, DesensitizeField annotation) {
+			return DesensitizedUtil.email(source);
+		}
+	},
 
     /** 账号 */
-    ACCOUNT,
+    ACCOUNT {
+		@Override
+		public String desensitive(String source, DesensitizeField annotation) {
+			return StrUtil.hide(source, 2, source.length());
+		}
+	},
 
     /** 密码 */
-    PASSWORD,
+    PASSWORD {
+		@Override
+		public String desensitive(String source, DesensitizeField annotation) {
+			return DesensitizedUtil.password(source);
+		}
+	},
     
     /** 银行卡 */
-    BANK_CARD,
+    BANK_CARD {
+		@Override
+		public String desensitive(String source, DesensitizeField annotation) {
+			return DesensitizedUtil.bankCard(source);
+		}
+	},
     
     /** 自定义（此项需设置脱敏的范围）*/
-    CUSTOMER,
+    CUSTOMER {
+		@Override
+		public String desensitive(String source, DesensitizeField annotation) {
+			int len = source.length();
+			int prefixLen = annotation.prefixLen();
+			int suffixLen = annotation.suffixLen();
+			String symbol = annotation.symbol();
+			return CharSequenceUtil.hide(source, prefixLen, len - suffixLen).replaceAll("\\*", symbol);
+		}
+	};
+	
+	public abstract Object desensitive(String source, DesensitizeField annotation);
+	
 }
